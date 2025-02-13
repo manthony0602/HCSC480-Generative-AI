6898c42437c88b3080ac3ddfc3a47f0164752924
manthony0602 <90815741+manthony0602@users.noreply.github.com>
2025-02-13 15:26:54 -0500

    Initial Commit

---
+++ Test/InputOutput-Testing.py
@@ -0,0 +1,5 @@
+month = input("Enter Month: ")
+day = input("Enter Day: ")
+year = input("Enter Year: ")
+
+print(month + " " + str(day) + ", " + str(year))
---
+++ Test/hello.py
@@ -0,0 +1,2 @@
+print("Hello, World! I am extremely excited to be here!")
+print("*This is just a test file to make sure everything is working*")
